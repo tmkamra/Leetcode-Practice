bab942d5449681d30a32c065c0ca04a066563055
tmkam <tmkam@LAPTOP-0KO43TV2>
2021-05-20 01:28:17 +0530

    1480. Running Sum of 1d Array

---
+++ Test/src/com/test/RunningSumArray.java
@@ -0,0 +1,29 @@
+package com.test;
+
+public class RunningSumArray {
+
+	public int[] runningSum(int[] nums) {
+      int[] sumArray=new int[nums.length];
+      
+      for(int i=0;i<nums.length;i++) {
+    	  if(i>0) {
+    		  sumArray[i]=sumArray[i-1]+nums[i];
+    	  } else {
+    		  sumArray[i]=nums[i];
+    	  }
+      }
+	  return sumArray;
+    }
+	
+	
+	public static void main(String[] args) {
+		RunningSumArray runningSumArray=new RunningSumArray();
+		int[] sumArray=new int[5];
+		sumArray=runningSumArray.runningSum(new int[] {3,1,2,10,1});
+		for(int i=0; i<sumArray.length; i++) {
+			System.out.println(sumArray[i]);
+		}
+
+	}
+
+}
